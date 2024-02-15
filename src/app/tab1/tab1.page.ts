e69ff0e4b4cfefa0aafa1442e802971f411dce9f
Suryanizam <suryanizamsabturani@gmail.com>
2024-02-15 20:47:54 +0800

    Changes to be committed: 	new file:   android/.gitignore 	new file:   android/app/.gitignore 	new file:   android/app/build.gradle 	new file:   android/app/capacitor.build.gradle 	new file:   android/app/proguard-rules.pro 	new file:   android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java 	new file:   android/app/src/main/AndroidManifest.xml 	new file:   android/app/src/main/java/io/ionic/starter/MainActivity.java 	new file:   android/app/src/main/res/drawable-land-hdpi/splash.png 	new file:   android/app/src/main/res/drawable-land-mdpi/splash.png 	new file:   android/app/src/main/res/drawable-land-xhdpi/splash.png 	new file:   android/app/src/main/res/drawable-land-xxhdpi/splash.png 	new file:   android/app/src/main/res/drawable-land-xxxhdpi/splash.png 	new file:   android/app/src/main/res/drawable-port-hdpi/splash.png 	new file:   android/app/src/main/res/drawable-port-mdpi/splash.png 	new file:   android/app/src/main/res/drawable-port-xhdpi/splash.png 	new file:   android/app/src/main/res/drawable-port-xxhdpi/splash.png 	new file:   android/app/src/main/res/drawable-port-xxxhdpi/splash.png 	new file:   android/app/src/main/res/drawable-v24/ic_launcher_foreground.xml 	new file:   android/app/src/main/res/drawable/ic_launcher_background.xml 	new file:   android/app/src/main/res/drawable/splash.png 	new file:   android/app/src/main/res/layout/activity_main.xml 	new file:   android/app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml 	new file:   android/app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml 	new file:   android/app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-hdpi/ic_launcher_foreground.png 	new file:   android/app/src/main/res/mipmap-hdpi/ic_launcher_round.png 	new file:   android/app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-mdpi/ic_launcher_foreground.png 	new file:   android/app/src/main/res/mipmap-mdpi/ic_launcher_round.png 	new file:   android/app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-xhdpi/ic_launcher_foreground.png 	new file:   android/app/src/main/res/mipmap-xhdpi/ic_launcher_round.png 	new file:   android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-xxhdpi/ic_launcher_foreground.png 	new file:   android/app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png 	new file:   android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_foreground.png 	new file:   android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png 	new file:   android/app/src/main/res/values/ic_launcher_background.xml 	new file:   android/app/src/main/res/values/strings.xml 	new file:   android/app/src/main/res/values/styles.xml 	new file:   android/app/src/main/res/xml/file_paths.xml 	new file:   android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java 	new file:   android/build.gradle 	new file:   android/capacitor.settings.gradle 	new file:   android/gradle.properties 	new file:   android/gradle/wrapper/gradle-wrapper.jar 	new file:   android/gradle/wrapper/gradle-wrapper.properties 	new file:   android/gradlew 	new file:   android/gradlew.bat 	new file:   android/settings.gradle 	new file:   android/variables.gradle 	modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/tab1/tab1.page.html 	modified:   src/app/tab1/tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -17,6 +17,8 @@ export class Tab1Page {
 
   nominal_mass_per_metre: any;
 
+  mass_tolerance: any;
+
   constructor() {
     this.nominal_diameter_list = ["10", "12", "16", "20", "25", "32"];
   }
@@ -47,12 +49,58 @@ export class Tab1Page {
     masspermetre[0].style.color = "red";
 
     this.mass_per_metre !== '' ? masspermetre[0].style.color = "green" : masspermetre[0].style.color = "red";
+
+    let nommasspermetre = document.getElementsByClassName('nom-mass-per-metre') as HTMLCollectionOf<HTMLElement>;
+
+    if( this.nominal_diameter === '10' ){
+      this.nominal_mass_per_metre = '0.617';
+    }else if( this.nominal_diameter === '12' ){
+      this.nominal_mass_per_metre = '0.888';
+    }else if( this.nominal_diameter === '16' ){
+      this.nominal_mass_per_metre = '1.580';
+    }else if( this.nominal_diameter === '20' ){
+      this.nominal_mass_per_metre = '2.470';
+    }else if( this.nominal_diameter === '25' ){
+      this.nominal_mass_per_metre = '3.850';
+    }else if( this.nominal_diameter === '32' ){
+      this.nominal_mass_per_metre = '6.310';
+    }else{
+      this.nominal_mass_per_metre = '';
+    }
+
+    nommasspermetre[0].style.color = "red";
+
+    this.nominal_mass_per_metre !== '' ? nommasspermetre[0].style.color = "green" : nommasspermetre[0].style.color = "red";
+
+    this.mass_tolerance = ((this.mass_per_metre - this.nominal_mass_per_metre) / this.nominal_mass_per_metre) * 100;
+
+    let masstolerance = document.getElementsByClassName('mass-tolerance') as HTMLCollectionOf<HTMLElement>;
+
+    !isNaN(this.mass_tolerance) ? this.mass_tolerance = this.mass_tolerance : this.mass_tolerance = '';
+    this.mass_tolerance !== Infinity ? this.mass_tolerance = this.mass_tolerance : this.mass_tolerance = '';
+    this.mass_tolerance !== 0 ? this.mass_tolerance = this.mass_tolerance : this.mass_tolerance = '';
+
+    masstolerance[0].style.color = "red";
+
+    this.mass_tolerance !== '' ? masstolerance[0].style.color = "green" : masstolerance[0].style.color = "red";
   }
 
   reset() {
     this.sample_weight = '';
     this.sample_length = '';
+    this.mass_per_metre = '';
     this.nominal_diameter = null;
+    this.nominal_mass_per_metre = '';
+    this.mass_tolerance = '';
+
+    let masspermetre = document.getElementsByClassName('mass-per-metre') as HTMLCollectionOf<HTMLElement>;
+    masspermetre[0].style.color = "red";
+
+    let nommasspermetre = document.getElementsByClassName('nom-mass-per-metre') as HTMLCollectionOf<HTMLElement>;
+    nommasspermetre[0].style.color = "red";
+
+    let masstolerance = document.getElementsByClassName('mass-tolerance') as HTMLCollectionOf<HTMLElement>;
+    masstolerance[0].style.color = "red";
   }
 
 }
