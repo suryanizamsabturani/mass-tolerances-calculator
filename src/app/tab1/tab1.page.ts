1fe6165ad84dd7709e884414f074584a25362750
Suryanizam <suryanizamsabturani@gmail.com>
2024-02-15 19:31:57 +0800

    Changes to be committed: 	modified:   src/app/tab1/tab1.page.html 	modified:   src/app/tab1/tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,17 +7,46 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  sample_weight: any;
-  sample_length: any;
+  sample_weight: any = '';
+  sample_length: any = '';
+  
+  mass_per_metre: any;
+
   nominal_diameter: any;
   nominal_diameter_list: any = [];
+
+  nominal_mass_per_metre: any;
 
   constructor() {
     this.nominal_diameter_list = ["10", "12", "16", "20", "25", "32"];
   }
 
+  numberOnlyValidation(event: any) {
+    const pattern = /[0-9.,]/;
+    let inputChar = String.fromCharCode(event.charCode);
+
+    if (!pattern.test(inputChar)) {
+      // invalid character, prevent input
+      event.preventDefault();
+    }
+  }
+
   calculate() {
-    
+    this.sample_weight === '' ? this.sample_weight = '' : this.sample_weight = this.sample_weight;
+
+    this.sample_length === '' ? this.sample_length = '' : this.sample_length = this.sample_length;
+
+    this.mass_per_metre = (this.sample_weight * 1000) / (this.sample_length * 1000);
+
+    let masspermetre = document.getElementsByClassName('mass-per-metre') as HTMLCollectionOf<HTMLElement>;
+
+    !isNaN(this.mass_per_metre) ? this.mass_per_metre = this.mass_per_metre : this.mass_per_metre = '';
+    this.mass_per_metre !== Infinity ? this.mass_per_metre = this.mass_per_metre : this.mass_per_metre = '';
+    this.mass_per_metre !== 0 ? this.mass_per_metre = this.mass_per_metre : this.mass_per_metre = '';
+
+    masspermetre[0].style.color = "red";
+
+    this.mass_per_metre !== '' ? masspermetre[0].style.color = "green" : masspermetre[0].style.color = "red";
   }
 
   reset() {
