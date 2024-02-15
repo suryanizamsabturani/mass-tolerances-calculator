db6e68eef74f4b232838dd056133afcce47d9862
Suryanizam <suryanizamsabturani@gmail.com>
2024-02-15 05:38:05 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mass Tolerances Calculator',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
